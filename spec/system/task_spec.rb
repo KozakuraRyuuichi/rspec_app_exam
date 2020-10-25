fe71cc688069d5baa530e98aee875f99924de6d5
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-25 17:43:42 +0900

    [update]「Taskを編集した場合、一覧画面で編集後の内容が表示されること」の変数を修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -59,11 +59,11 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task編集' do
+    let(:project) { create(:project) }
+    let(:task) { create(:task, project_id: project.id) }
     context '正常系' do
       it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
         visit edit_project_task_path(project, task)
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
